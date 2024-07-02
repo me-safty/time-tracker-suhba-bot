a2fc01d4ba5eb9a44dd70995ab1fbe3b06c10c91
mohamed.safty@team-parallax.com <safty@localhost.localdomain>
2024-07-02 23:41:32 +0300

    reply and send message

--- app/src/methods/sendMessage.js
+++ app/src/methods/sendMessage.js
@@ -4,8 +4,15 @@ export const sendMessage = async (msg, match) => {
 	const chatId = msg.chat.id;
 	const messageId = msg.message_id;
 	const value = match[1]
+
 	bot.deleteMessage(chatId, messageId).catch((error) => {
-    console.error('Failed to delete message:', error);
-  });
-	bot.sendMessage(chatId, value);	
+		console.error('Failed to delete message:', error);
+	});
+
+	if (msg.reply_to_message) {
+    const repliedMessageId = msg.reply_to_message.message_id;
+    bot.sendMessage(chatId, value, { reply_to_message_id: repliedMessageId });
+  } else {
+		bot.sendMessage(chatId, value);	
+  }
 }
