d0a62d33b4f3371c0d67f3a76891264550361908
mohamed.safty@team-parallax.com <safty@localhost.localdomain>
2024-08-29 17:56:12 +0300

    feat: edit add time

--- app/src/methods/addTime.js
+++ app/src/methods/addTime.js
@@ -33,7 +33,7 @@ export const addTime = async (msg, match) => {
 				// 	await changeCustomTitle(chatId, userId, rankName)
 				// }
 
-				const randomMessage = Math.floor(Math.random() * 40) === 4 || userId === mohamedSaftyId
+				const randomMessage = Math.floor(Math.random() * 40) === 4
 					? `جزاك الله خيرا يا ${rankName.split(" ")[0]} ${user.name.split(" ")[0]} `
 					: ""
 				const addTimeMessage = `<strong>إنجازك اليوم: ${todayTime}د
