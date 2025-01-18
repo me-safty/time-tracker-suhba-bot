e4c776c548a1482ac86387961bf72b6d48622602
mohamed.safty@team-parallax.com <safty@localhost.localdomain>
2025-01-19 00:19:07 +0200

    feat: fix challenge time

--- app/src/db/challenge/getActiveChallenge.js
+++ app/src/db/challenge/getActiveChallenge.js
@@ -1,10 +1,11 @@
 import { client } from "../../sanityClient"
+import { convertToGMTPlus3 } from "../../util"
 
 export const getActiveChallenge = async (enableTodayChallengeCheck = true) => {
 	const activeChallenge = await client.fetch(`*[_type == 'challenge' && !isFinished][0]`)
 	if (enableTodayChallengeCheck && activeChallenge) {
 		const activeChallengeDateImMS = new Date(activeChallenge?.challengeId).getTime()
-		if (new Date().getTime() < activeChallengeDateImMS) {
+		if (convertToGMTPlus3(new Date()).getTime() < activeChallengeDateImMS) {
 			return null
 		} else {
 			return activeChallenge
